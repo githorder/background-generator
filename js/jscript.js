e03eda26510d3f94d1a37807fba957fa05bf0e4b
Universe200 <40492265+Universe200@users.noreply.github.com>
2018-10-14 01:46:26 +0500

    Adding starting project

---
+++ js/jscript.js
@@ -0,0 +1,24 @@
+﻿const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
+const color3 = document.querySelector('.color3');
+const body = document.getElementById('gradient');
+
+const setGradient = () => {
+    body.style.background =
+        'linear-gradient(to right, ' +
+        color1.value +
+        ', ' +
+        color2.value +
+        ', ' +
+        color3.value +
+        ')';
+    css.textContent = body.style.background;
+}
+
+
+color1.addEventListener('input', setGradient);
+
+color2.addEventListener('input', setGradient);
+
+color3.addEventListener('input', setGradient);
